663219072b4e3664eb49aca2cc671c57237d7a0b
Hanno Heinrichs <hanno.heinrichs@rwth-aachen.de>
2016-04-26 18:17:28 +0200

    clean up code

--- VBoxExtPackUpdater.py
+++ VBoxExtPackUpdater.py
@@ -1,88 +1,74 @@
 #!/usr/bin/env python2
 
-import requests
-from bs4 import BeautifulSoup
-from urlparse import urljoin
+import argparse
 import hashlib
-import tempfile
-import subprocess
 import os
 import re
-# from IPython import embed
+import subprocess
+import tempfile
+import requests
+from bs4 import BeautifulSoup
 
-# https://www.virtualbox.org/download/hashes/5.0.18/SHA256SUMS
-HASH_BASE_URL = ('https://www.virtualbox.org/download/hashes/{version}/'
-                 'SHA256SUMS')
-
-# http://download.virtualbox.org/virtualbox/5.0.18/Oracle_VM_VirtualBox_Extension_Pack-5.0.18-106667.vbox-extpack
-DL_BASE_URL = 'http://download.virtualbox.org/virtualbox/'
-
-
-def getLatestVersion():
-    r = requests.get(urljoin(DL_BASE_URL, 'LATEST.TXT'))
-    return r.text.strip()
+HASH_URL = 'https://www.virtualbox.org/download/hashes/{version}/SHA256SUMS'
+DL_BASE_URL = 'http://download.virtualbox.org/virtualbox/{version}/'
 
 
-def getInstalledVersion():
+def get_installed_version():
     version = subprocess.check_output(['VBoxManage', '--version'])
-    # needed, as some VBoxManage warnings also go to stdout
+    # needed, as some/all(?) VBoxManage warnings go to stdout as well
     return re.search('^(\d+\.\d+\.\d+)r\d+$', version, re.MULTILINE).group(1)
 
 
-def getSHA256SUMS(version):
-    r = requests.get(
-        HASH_BASE_URL.format(version=version),
-        allow_redirects=False
-    )
+def get_sha256sums(version):
+    r = requests.get(HASH_URL.format(version=version), allow_redirects=False)
     return r.text
 
 
-def loadExtPack(version):
-    extpack_dir = urljoin(
-        DL_BASE_URL, version
-    ) + '/'
+def load_ext_pack(version):
+    extpack_dir = DL_BASE_URL.format(version=version)
     r = requests.get(extpack_dir)
     soup = BeautifulSoup(r.text, 'lxml')
     extpack_name = filter(
         lambda x: x.endswith('.vbox-extpack'),
         [x['href'] for x in soup.findAll('a')]
     )[0]
-    extpack_url = urljoin(extpack_dir, extpack_name)
+    extpack_url = extpack_dir + extpack_name
     r = requests.get(extpack_url)
     return {'filename': extpack_name, 'data': r.content}
 
 
-def verifyExtPack(extpack, SHA256SUMS):
-    ist_sha256 = hashlib.sha256(extpack['data']).hexdigest()
+def verify_ext_pack(extpack, sha256sums):
+    computed_sha256 = hashlib.sha256(extpack['data']).hexdigest()
     regex = '^([^\s]+)[\s*]+{filename}$'.format(
         filename=re.escape(extpack['filename'])
     )
-    soll_sha256 = re.match(regex, SHA256SUMS, re.MULTILINE).group(1)
-    return soll_sha256 == ist_sha256
+    good_sha256 = re.match(regex, sha256sums, re.MULTILINE).group(1)
+    return computed_sha256 == good_sha256
 
 
-def installExtPack(extpack):
+def install_ext_pack(extpack):
     tmp_dir = tempfile.mkdtemp()
     tmp_file = os.path.join(tmp_dir, extpack['filename'])
     with open(tmp_file, 'w+b') as f:
         f.write(extpack['data'])
 #    subprocess.call(['sha256sum', tmp_file])
-    subprocess.call([
-        'sudo', 'VBoxManage', 'extpack', 'install', tmp_file
-    ])
+    subprocess.call(['sudo', 'VBoxManage', 'extpack', 'install', tmp_file])
     os.remove(tmp_file)
     os.rmdir(tmp_dir)
 
 
-def buildKernelModules():
-    subprocess.call(['sudo', '/sbin/rcvboxdrv', 'setup'])
+def build_kernel_modules():
+    subprocess.call(['sudo', 'rcvboxdrv', 'setup'])
 
 
 if __name__ == '__main__':
-    version = getInstalledVersion()
-#    version = getLatestVersion()
-    SHA256SUMS = getSHA256SUMS(version)
-    extpack = loadExtPack(version)
-    if verifyExtPack(extpack, SHA256SUMS):
-        installExtPack(extpack)
-#    buildKernelModules()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--build-kernel-modules', action="store_true")
+    args = parser.parse_args()
+    if args.build_kernel_modules:
+        build_kernel_modules()
+    version = get_installed_version()
+    sha256sums = get_sha256sums(version)
+    extpack = load_ext_pack(version)
+    if verify_ext_pack(extpack, sha256sums):
+        install_ext_pack(extpack)
